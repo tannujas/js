76573c6f349c265df350c1e0338931af85de17bb
tannujas <tannujas50@gmail.com>
2025-09-28 15:48:14 +0000

    conversion data type

---
+++ .devcontainer/01_basic/conversionoperation.js
@@ -0,0 +1,63 @@
+/*let score = 33
+console.log(typeof score);
+console.log(typeof (score)); //you can write also it in Parentheses() 
+console.log(score) */  // 33
+
+/*let score = "33"
+let valueInNumber = Number(score) //value convert in number
+console.log(typeof (valueInNumber)); */
+
+//if you want that what is the value
+/*let score = "33abc"
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber) */  // NaN
+
+/* let score = null
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber) */ //0
+
+/* let score = undefined
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber) */ //nan
+
+/* let score = true
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber) */ // true = 1 and false = 0
+
+/* let score = "Tannu"
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber)  */ // nan
+
+/*let isloginId = 1
+let booleanIsLoggedIn = Boolean(isloginId)
+console.log(typeof (booleanIsLoggedIn));
+console.log(booleanIsLoggedIn)*/ // 1 = true 
+
+/* let isloginId = ""
+let booleanIsLoggedIn = Boolean(isloginId)
+console.log(typeof (booleanIsLoggedIn));
+console.log(booleanIsLoggedIn)  */ //  "" = false
+
+/*let isloginId = "Tannu"
+let booleanIsLoggedIn = Boolean(isloginId)
+console.log(typeof (booleanIsLoggedIn));
+console.log(booleanIsLoggedIn) */ // "Tannu" = true
+
+/*let isloginId = 0
+let booleanIsLoggedIn = Boolean(isloginId)
+console.log(typeof (booleanIsLoggedIn));
+console.log(booleanIsLoggedIn)*/ // 0 = false
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(typeof (stringNumber));
+console.log(stringNumber) // 33 = string
+
+
+
+
