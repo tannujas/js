2ff1ed242db7e9fc28725ca142f1032c99c694b1
tannujas <tannujas50@gmail.com>
2025-09-28 14:51:36 +0000

    Datatypes

---
+++ .devcontainer/01_basic/Datatype.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as new version
+//alert(3 + 3) //we are using nodejs, not browser
+
+//Data type 
+
+let name = "Tannu"
+let age = 22
+let isloginId = true
+
+
+/* ->premitive data type */
+
+//number => 2 to power 53
+//string
+//boolean => true/false
+//bigInt
+//null =>standlone value (and null is object)
+//example let state = "null"
+//undefined
+//example let state;
+//symbol =>unique
+
+//object
+
+console.log(typeof "Tannu")
+console.log(typeof 18)
+console.log(typeof true)
+
+//interesting part 
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
