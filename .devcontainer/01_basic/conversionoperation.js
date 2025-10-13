f58b2ee329117f90a03cf870dc14ce5f12b99612
tannujas <tannujas50@gmail.com>
2025-10-13 14:22:20 +0000

    convnersionoperation

--- .devcontainer/01_basic/conversionoperation.js
+++ .devcontainer/01_basic/conversionoperation.js
@@ -53,11 +53,47 @@ let booleanIsLoggedIn = Boolean(isloginId)
 console.log(typeof (booleanIsLoggedIn));
 console.log(booleanIsLoggedIn)*/ // 0 = false
 
-let somenumber = 33
+/* let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(typeof (stringNumber));
-console.log(stringNumber) // 33 = string
+console.log(stringNumber) // 33 = string */
 
 
+/************************ Operations ***************/
 
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2%3);
 
+/* let str1 = "Hello"
+let str2 = " Jas"
+let str3 = str1 + str2
+console.log(str3);  // Hello Jas */
+
+/* console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); //if String is first then every value count as a string - 122
+console.log(1 + 2 + "2"); // if string is last then first  values will conversion - 32 */
+
+//console.log(+true);
+//console.log(+ "");
+
+//let num1 , num2 , num3
+// num1 = num2 = num3 = 2 + 2
+
+/* let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); */
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+//https://tc39.es/ecma262/#sec-type-conversion
+
+//link to study
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
