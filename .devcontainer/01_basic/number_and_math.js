84efb137558b0c76dbed050458ffacc2caf83a5f
tannujas <tannujas50@gmail.com>
2025-10-14 15:28:58 +0000

    numerandmath

---
+++ .devcontainer/01_basic/number_and_math.js
@@ -0,0 +1,53 @@
+/*const score = 400
+console.log(score);
+
+const balance = new Number(100) //we define the number type main hi mile
+console.log(balance); */
+
+/* const balance = new Number(100) 
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed());
+console.log(balance.toFixed(2));
+
+//const othernumber = 23.8966
+const othernumber = 123.8966
+console.log(othernumber.toPrecision(3));
+console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log (hundreds.toLocaleString()); // 1,000,000 it is US type
+console.log (hundreds.toLocaleString('en-IN')); // 10,00,000 it is indian type */
+
+
+/* --------------- Maths -------------- */
+/* console.log(Math);// Math self is a object
+console.log(Math.abs(-4)); //absolute -- negative value convert positive and positive value always in positive
+console.log(Math.abs(4));
+console.log(Math.round(4.5));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2)); //take the upervalue
+console.log(Math.floor(4.9));//take the lowervalue
+console.log(Math.min(3,4,5));
+console.log(Math.max(3,4,5)); */
+
+console.log(Math.random()); //value always come in between 0 to 1
+console.log((Math.random()*10)+ 1); //10 meaning where should the value be exmaple - dashed game value have last 1 to 6 --- * 6
+console.log(Math.floor(Math.random()*10)+ 1); //only need one number
+
+
+const min = 10
+const max = 20
+console.log(Math.floor())
+
+
+
+
+
+
+
+
+
+
+
+
