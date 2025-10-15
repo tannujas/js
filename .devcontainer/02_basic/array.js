40a8ab774d502b6e9cc621634ec37046f8f89bcf
tannujas <tannujas50@gmail.com>
2025-10-15 05:38:58 +0000

    array

---
+++ .devcontainer/02_basic/array.js
@@ -0,0 +1,16 @@
+//array methods
+
+let Fruits = ["Apply" , "Mango" , "Orange"];
+Fruits.push("Banana") ;
+console.log(Fruits);
+Fruits.pop();
+console.log(Fruits);
+Fruits.unshift("Banana");
+console.log(Fruits);
+Fruits.shift();
+console.log(Fruits);
+Fruits.indexOf(2);
+console.log(Fruits);
+Fruits.includes("Banana");
+console.log(Fruits);
+console.log(Fruits.length);
