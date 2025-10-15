4eb269ddff212348663c423970f01f7d1754b673
tannujas <tannujas50@gmail.com>
2025-10-15 16:48:53 +0000

    array

--- .devcontainer/02_basic/array.js
+++ .devcontainer/02_basic/array.js
@@ -1,16 +1,36 @@
 //array methods
 
-let Fruits = ["Apply" , "Mango" , "Orange"];
-Fruits.push("Banana") ;
+let Fruits = ["Apply" , "Mango" , "Orange", "guava"];
+
+/*Fruits.push("Banana");
+console.log(Fruits);*/
+
+/*Fruits.pop();
+console.log(Fruits);*/
+
+/*Fruits.unshift("Banana");
+console.log(Fruits); */
+
+/*Fruits.shift();
+console.log(Fruits);*/
+
+/*console.log(Fruits.indexOf("Mango"));
+console.log(Fruits.includes("Banana"));
+console.log(Fruits.length);*/
+
+
+/* const Vegetable = Fruits.join();
 console.log(Fruits);
-Fruits.pop();
-console.log(Fruits);
-Fruits.unshift("Banana");
-console.log(Fruits);
-Fruits.shift();
-console.log(Fruits);
-Fruits.indexOf(2);
-console.log(Fruits);
-Fruits.includes("Banana");
-console.log(Fruits);
-console.log(Fruits.length);
+console.log(typeof Vegetable); */
+
+//slice , Splice
+// Interview question what is the different between slice and splice
+
+/* console.log("A", Fruits);
+const myn1 = Fruits.slice(1 , 3)
+console.log(myn1);
+
+console.log("B", Fruits);
+const myn2 = Fruits.splice(1, 3 , "kiwi", "date",  "cherry")
+console.log("C" , Fruits);
+console.log(myn2);*/
