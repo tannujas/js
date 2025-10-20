4bc3ea6eeea78747622c6282e653e03ca7d9d525
tannujas <tannujas50@gmail.com>
2025-10-20 07:50:25 +0000

    array

--- .devcontainer/02_basic/array.js
+++ .devcontainer/02_basic/array.js
@@ -34,3 +34,36 @@ console.log("B", Fruits);
 const myn2 = Fruits.splice(1, 3 , "kiwi", "date",  "cherry")
 console.log("C" , Fruits);
 console.log(myn2);*/
+
+
+/*************** Array Part 2 *******************/
+
+const marvel_heros = ["thor", "Ironman", "Spiderman"]
+const dc_heros = ["superman", "Flash", "Batman"]
+
+//marvel_heros.push(dc_heros) //put only existing aaray
+//console.log(marvel_heros);
+
+const allHeros = marvel_heros.concat(dc_heros) //put new array
+console.log(allHeros);
+
+// Spread operator (Failana)
+
+const new_all_heros =[...marvel_heros, ...dc_heros]
+console.log(new_all_heros);
+
+//output - both are working same concat, spread[ 'thor', 'Ironman', 'Spiderman', 'superman', 'Flash', 'Batman' ]
+//[ 'thor', 'Ironman', 'Spiderman', 'superman', 'Flash', 'Batman' ]
+
+/*const another_array = [1 ,2 , 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array); */
+
+/*console.log(Array.isArray("Tannu"))
+console.log(Array.from("Tannu")) //convert value in array
+consol.log(Array.from({name: Tannu})) //Interesting case meaning of that it can't be convert in array alway provide the output [] empty */
+
+let score1  = 100;
+let score2  = 200;
+let score3  = 300;
+console.log(Array.of(score1, score2, score3));//creates a new array from the values you give.
